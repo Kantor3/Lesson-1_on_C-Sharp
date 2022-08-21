addd1d5fb7f1d4bc8bbd8db114269052308253e1
kantor <kantor3@yandex.ru>
2022-08-21 15:06:32 +0500

    sulution of task-2 of the homework task-2

---
+++ Homework_Tasks/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+1.	Задача 2: Напишите программу, которая на вход принимает 
+    два числа и выдаёт, какое число большее, а какое меньшее.
+-------------------------------------------------------------
+*/
+Console.WriteLine("Input any two number:");
+Console.WriteLine("---");
+
+Console.Write("Input the first number => ");
+double number_1 = Convert.ToDouble(Console.ReadLine().Replace(".", ","));
+Console.Write("Input the second number => ");
+double number_2 = Convert.ToDouble(Console.ReadLine().Replace(".", ","));
+
+if (number_1 > number_2)
+{
+    Console.WriteLine($"First number = {number_1} > Second = {number_2}");
+}
+else
+{
+    Console.WriteLine($"Second number = {number_2} > First = {number_1}");
+}
